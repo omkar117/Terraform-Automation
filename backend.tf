8fb091131ebd09901e7dee9aa88aab81179ea058
omkar117 <140892815+omkar117@users.noreply.github.com>
2024-01-02 11:51:46 +0530

    Update backend.tf
    Changed the bucket name

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-21"
+    bucket = "ym-tf"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
